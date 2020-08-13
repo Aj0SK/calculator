05e7a2ac128e5fefba8c3435a79d87abce281999
Aj0SK <andrejkorman@gmail.com>
2020-08-13 09:53:18 +0200

    Refactor tests

--- test/sample1_unittest.cpp
+++ test/sample1_unittest.cpp
@@ -1,38 +1,37 @@
 #include "gtest/gtest.h"
 #include "../src/driver.hh"
 
+void test_inputs(const std::vector<std::pair<std::string, double> >& tests)
+{
+    driver drv;
+    for(const auto& test : tests)
+    {
+        drv.parse_string (test.first);
+        EXPECT_EQ(drv.result, test.second);
+    }
+}
+
 namespace
 {
     TEST(ADD, Trivial)
     {
-        driver drv;
-
-        drv.parse_string ("20+30+15+10\n");
-        EXPECT_EQ(drv.result, 75);
-
-        drv.parse_string ("20+30+15+10.0\n");
-        EXPECT_EQ(drv.result, 75.0);
-
-        drv.parse_string ("1.5+10.0+10.0\n");
-        EXPECT_EQ(drv.result, 21.5);
-
-        drv.parse_string ("1.5 + (-0.5)\n");
-        EXPECT_EQ(drv.result, 1.0);
-
-        drv.parse_string ("1 + (-1)\n");
-        EXPECT_EQ(drv.result, 0);
+        std::vector<std::pair<std::string, double> > tests = {
+            {"20+30+15+10\n", 75},
+            {"20+30+15+10.0\n", 75.0},
+            {"1.5+10.0+10.0\n", 21.5},
+            {"1.5 + (-0.5)\n", 1.0},
+            {"1 + (-1)\n", 0}
+        };
+        test_inputs(tests);
     }
     TEST(SUBTRACT, Trivial)
     {
-        driver drv;
-
-        drv.parse_string ("20-10\n");
-        EXPECT_EQ(drv.result, 10);
-
-        drv.parse_string ("1-0.5\n");
-        EXPECT_EQ(drv.result, 0.5);
-
-        drv.parse_string ("21.0-1.0\n");
-        EXPECT_EQ(drv.result, 20.0);
+        std::vector<std::pair<std::string, double> > tests = {
+            {"20-10\n", 10},
+            {"1-0.5\n", 0.5},
+            {"21.0-1.0\n", 20.0},
+            {"21.0-(-1.0)\n", 22.0},
+        };
+        test_inputs(tests);
     }
 }
